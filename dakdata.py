7bfb2628ecae1c2e4c25a95353566dc1f2de5a16
Martin Green <martin@famgreen.se>
2016-10-25 21:54:43 +0200

    Fixed import: * automatic detection of orgnr and foreningsid, removed quotes * added warning if no troop is selected * delete all data does nothing (safety) * added files for Visual Studio Code (debugging support!)

---
+++ dakdata.py
@@ -0,0 +1,66 @@
+﻿# -*- coding: utf-8 -*-
+import datetime
+
+# http://www.sverigesforeningssystem.se/dak-formatet/vad-ar-dak/
+
+class Deltagare:
+	id = ""
+	Foernamn = ""
+	Efternamn = ""
+	Personnummer = ""
+	Kon = "" # Man/Kvinna
+	Ledare = False
+	
+	def __init__(self, id, Foernamn, Efternamn, Personnummer, female, ledare):
+		self.id = id
+		self.Foernamn = Foernamn
+		self.Efternamn = Efternamn
+		self.Personnummer = Personnummer
+		self.Kon = "Kvinna" if female else "Man"
+		self.Ledare = ledare
+
+class Sammankomst:
+	kod = ""
+	datum = None
+	duration = 90
+	Aktivitet = "Moete" # OBS en av: Traening, Match, Moete, Oevrigt
+	deltagare = []
+	ledare = []
+	
+	def __init__(self, kod, datum, duration, Aktivitet):
+		self.kod = kod
+		self.datum = datum
+		self.duration = duration
+		self.Aktivitet = Aktivitet
+		self.deltagare = []
+		self.ledare = []
+
+	def GetDateString(self):
+		return self.datum.strftime('%Y-%m-%d')
+	
+	def GetStartTimeString(self):
+		return self.datum.strftime('%H:%M')
+		
+	def GetStopTimeString(self):
+		endtime = self.datum + datetime.timedelta(minutes=self.duration)
+		return endtime.strftime('%H:%M')
+
+class Narvarokort:
+	NaervarokortNummer="1"
+	Sammankomster = []
+	Aktivitet="Scouting"
+	Lokal="Scouthuset"
+	NamnPaaKort=""
+	deltagare = []
+	ledare = []
+	
+	
+class DakData:
+	kommunID = "1480"
+	foreningsID = "3843"
+	foereningsNamn=u""
+	organisationsnummer="857203-7722"
+	
+	def __init__(self):
+		self.kort = Narvarokort()
+	
---
+++ pydev_startup.py
@@ -0,0 +1,9 @@
+import sys
+import os
+#Assuming that pdvsd is located in the working folder
+sys.path.append(os.getcwd()) 
+import ptvsd
+#Fee free to change the secret and port number 
+ptvsd.enable_attach(secret = 'gae', address = ('0.0.0.0', 3000))
+#The debug server has started and you can now use VS Code to attach to the application for debugging
+print("Google App Engine has started, ready to attach the debugger")
