2c6f9d7dd0a7726e8ceb377d07ba9da4fd857b4a
Thorbjörn Sundragon <puggan@spiro.se>
2020-06-19 22:37:57 +0200

    app: set up the /dbtest

--- app/api/DbTest.py
+++ app/api/DbTest.py
@@ -1,6 +1,23 @@
+import os
+import pymongo
 from flask_restful import Resource
+
 
 class DbTest(Resource):
     def get(self):
-        # TODO: connect to database
-        return {'ok': 'false'}
+        try:
+            authed_uri = os.getenv("MONGO_URI") % (os.getenv("MONGO_USER"), os.getenv("MONGO_PASSWORD"))
+            client = pymongo.MongoClient(authed_uri)
+            testdb = client["test"]["test"]
+            query = {"id": "test"}
+            row = testdb.find_one(query)
+            if row is None:
+                testdb.insert({"id": "test", "count": 1})
+            else:
+                testdb.update_one(query, {"$inc": {"count": 1}})
+            row = testdb.find_one(query)
+            if row is None:
+                return {"ok": False, "error": "empty database"}
+            return {"ok": True, "count": row["count"]}
+        except Exception as e:
+            return {"ok": False, "error": str(e)}
