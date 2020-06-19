49903af47f0a120ca158ec1e3b8c94f6b8a95980
Thorbjörn Sundragon <puggan@spiro.se>
2020-06-19 19:25:03 +0200

    front: add test of rest-api

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,5 +1,24 @@
 /// <reference path="./script.d.ts" />
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     console.log('script.ts loaded');
+    const pingElement = document.getElementById("ping") as HTMLParagraphElement;
+
+    const fetchPromise = fetch("/ping");
+    try {
+        await fetchPromise;
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+    const response = await fetchPromise;
+    const jsonPromise = response.json();
+    try {
+        await jsonPromise;
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+    const json = await jsonPromise;
+    pingElement.innerText = json.ping;
 });
