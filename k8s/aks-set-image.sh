d42cf342372f05491c9e1bc8594bdfd9a1fc379a
Thorbjörn Sundragon <puggan@spiro.se>
2020-06-24 19:33:13 +0200

    split live and staging

--- k8s/aks-set-image.sh
+++ k8s/aks-set-image.sh
@@ -1,3 +1,9 @@
 sed aks.yaml.template \
     -e "s@%IMAGE%@$PIPELINE_ACR/$SYSTEM_TEAMPROJECT:$BUILD_SOURCEBRANCHNAME.$BUILD_SOURCEVERSION@" \
+    -e "s@%NAMESPACE%@skojjt@" \
     > aks.yaml
+
+sed aks.yaml.template \
+    -e "s@%IMAGE%@$PIPELINE_ACR/$SYSTEM_TEAMPROJECT:$BUILD_SOURCEBRANCHNAME.$BUILD_SOURCEVERSION@" \
+    -e "s@%NAMESPACE%@skojjt-staging@" \
+    > aks.staging.yaml
