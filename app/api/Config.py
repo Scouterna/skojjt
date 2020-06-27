096a52aa1c1cbc9cb7f4f96a3c2c4545a10fa006
Thorbjörn Sundragon <puggan@spiro.se>
2020-06-28 01:57:23 +0200

    let /api/config return a list of semesters and karar

--- app/api/Config.py
+++ app/api/Config.py
@@ -1,7 +1,22 @@
 import os
 from auth.XOrigin import XOrigin
+from db import db
+from datetime import datetime
 
 
 class ConfigResource(XOrigin):
     def get(self):
-        return {"ok": True, "jwturl": os.getenv("JWT_URL")}
+        karer = {}
+        for kar in db['kar_names'].find():
+            karer[kar['_id']] = kar['name']
+        semesters = {}
+        time = datetime.now()
+        for year in range(2018, time.year):
+            year_str = str(year)
+            semesters[year_str + '01'] = 'VT-' + year_str
+            semesters[year_str + '07'] = 'HT-' + year_str
+        year_str = str(time.year)
+        semesters[year_str + '01'] = 'VT-' + year_str
+        if time.month > 6:
+            semesters[year_str + '07'] = 'HT-' + year_str
+        return {'ok': True, 'jwturl': os.getenv('JWT_URL'), 'karer': karer, 'semesters': semesters}
