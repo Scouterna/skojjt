58b46278fc5347237465053e2308252bff6f6da1
Thorbjörn Sundragon <puggan@spiro.se>
2020-06-27 13:23:00 +0200

    add /api/config and /saml

--- app/app.py
+++ app/app.py
@@ -1,19 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_restful import Resource, Api
 
+from api.Config import ConfigResource
 from api.DbTest import DbTest
 from api.jwt.VerifyToken import VerifyToken
 
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(ConfigResource, '/api/config')
 api.add_resource(DbTest, '/api/dbtest')
 api.add_resource(VerifyToken, '/api/jwt/verify_token')
 
-
-class Ping(Resource):
-    def get(self):
-        return {'ping': 'pong'}
-
-
-api.add_resource(Ping, '/ping')
+# if we are not logged in, we make a fake saml-session, that end at /saml, so accept and ignore it, and return to /
+@app.route('/saml')
+def samlRedirect():
+    return redirect('/');
