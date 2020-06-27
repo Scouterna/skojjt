199c5ac61598f7132afe95d18e13570ce6eeb995
Thorbjörn Sundragon <puggan@spiro.se>
2020-06-27 14:15:15 +0200

    Move token verification to class Auth

--- app/api/jwt/VerifyToken.py
+++ app/api/jwt/VerifyToken.py
@@ -1,8 +1,8 @@
-import os
 from flask import request
 from flask_restful import Resource
-from urllib.parse import urlparse
 import jwt
+
+from auth.Auth import Auth
 
 
 class VerifyToken(Resource):
@@ -26,15 +26,7 @@ class VerifyToken(Resource):
             return self.error("Authorization header is not Bearer type"), 403
 
         try:
-            public_key = open(os.getenv("JWT_PUBLIC_KEY_FILE")).read()
-        except Exception:
-            return self.error("Public key for JWT is missing on server"), 500
-
-        issuer = urlparse(os.getenv("JWT_URL")).hostname
-        audience = os.getenv("HOST_NAME")
-
-        try:
-            payload = jwt.decode(auth_header[7:], public_key, algorithms=["RS256"], issuer=issuer, audience=audience)
+            auth = Auth(auth_header[7:])
         except jwt.ExpiredSignatureError:
             return self.error("Token Expired"), 498
         except jwt.DecodeError as e:
@@ -42,30 +34,12 @@ class VerifyToken(Resource):
         except jwt.InvalidTokenError as e:
             return self.error("Invalid JWT-token, " + str(e)), 403
         except Exception as e:
-            return self.error("Failed to decode JWT, unknown Exception: " + type(e) + " -> " + str(e)), 403
+            return self.error("Failed to decode JWT, unknown Exception: " + str(type(e)) + " -> " + str(e)), 403
 
-        if "aud" not in payload:
-            return self.error("Payload have no aud-field", payload), 403
-
-        if "sub" not in payload:
-            return self.error("Payload have no sub-field", payload), 403
-
-        if payload["aud"] != os.getenv("HOST_NAME"):
-            return self.error("Payload have bad aud-field", payload), 403
-
-        try:
-            user_id_string, user_source = payload["sub"].split("@", 1)
-            user_id = int(user_id_string)
-        except ValueError:
-            return self.error("Payload have bad sub-field", payload), 403
-
-        if user_source != os.getenv("JWT_SOURCE_HOST"):
-            return self.error("Payload have bad sub-field", payload), 403
-
-        admin = "roles" in payload and "organisation:692:scoutid_admin" in payload["roles"]
+        admin = "roles" in auth.payload and "organisation:692:scoutid_admin" in auth.payload["roles"]
 
         return {
-                   "user": user_id,
+                   "user": auth.user_id,
                    "admin": admin,
-                   "data": payload,
+                   "data": auth.payload,
                }
---
+++ app/auth/Auth.py
@@ -0,0 +1,48 @@
+import os
+from urllib.parse import urlparse
+
+import jwt
+from jwt import InvalidTokenError
+
+
+class Auth:
+    token = ''
+    payload = None
+    user_id = None
+
+    @staticmethod
+    def read_token(token):
+        return Auth(token)
+
+    def __init__(self, token):
+        self.token = token
+        self.parse_token()
+
+    def parse_token(self):
+        try:
+            public_key = open(os.getenv('JWT_PUBLIC_KEY_FILE')).read()
+        except Exception:
+            raise EnvironmentError('Public key for JWT is missing on server')
+
+        issuer = urlparse(os.getenv('JWT_URL')).hostname
+        audience = os.getenv('HOST_NAME')
+
+        self.payload = jwt.decode(self.token, public_key, algorithms=['RS256'], issuer=issuer, audience=audience)
+
+        if 'aud' not in self.payload:
+            raise InvalidTokenError('Payload have no aud-field')
+
+        if 'sub' not in self.payload:
+            raise InvalidTokenError('Payload have no sub-field')
+
+        if self.payload['aud'] != os.getenv('HOST_NAME'):
+            raise InvalidTokenError('Payload have bad aud-field')
+
+        try:
+            user_id_string, user_source = self.payload['sub'].split('@', 1)
+            self.user_id = int(user_id_string)
+        except ValueError:
+            raise InvalidTokenError('Payload have bad sub-field')
+
+        if user_source != os.getenv('JWT_SOURCE_HOST'):
+            raise InvalidTokenError('Payload have bad sub-field')
